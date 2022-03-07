992efcb5d73e34689f620d2f8e4188fd769fa51d
Rex Yu <yu.chenhang@gmail.com>
2022-03-07 23:06:50 +0800

    init 交易模組

---
+++ trade/src/main/java/tw/com/rex/microservicepractice/trade/TradeApplication.java
@@ -0,0 +1,15 @@
+package tw.com.rex.microservicepractice.trade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class TradeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradeApplication.class, args);
+    }
+
+}
